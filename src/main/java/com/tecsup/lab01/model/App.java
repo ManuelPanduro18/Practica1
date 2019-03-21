7f100793f4da465ccb960d23455f2a4c58d6c503
ManuelPanduro18 <edson.panduro@tecsup.edu.pe>
2019-03-21 16:44:42 -0500

    Declaramos variable

--- src/main/java/com/tecsup/lab01/model/App.java
+++ src/main/java/com/tecsup/lab01/model/App.java
@@ -4,10 +4,9 @@ package com.tecsup.lab01.model;
  * Hello world!
  *
  */
-public class App 
+public class App extends com.tecsup.lab01.model.User
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
     }
 }
---
+++ src/main/java/com/tecsup/lab01/model/User.java
@@ -0,0 +1,7 @@
+package com.tecsup.lab01.model;
+
+public class User {
+	String nombre;
+	String apellidos;
+	int edad;
+}
